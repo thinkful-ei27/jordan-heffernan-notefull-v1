46c3c8ac7a7fbaf9a22418beb131bf9c64319054
Jordan Heffernan <jheff2@gmail.com>
2018-12-17 11:44:45 -0800

    added express server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 'use strict';
 
 // Load array of notes
-const data = require('./db/notes');
+// const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+// console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+const data = require ('./db/notes');
+
+const app = express();
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
