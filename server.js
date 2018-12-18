5530e5bc03c1f8d793ee13a120474b246a3b0a0b
Jordan Heffernan <jheff2@gmail.com>
2018-12-18 11:09:26 -0800

    added port module

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 'use strict';
 
-// Load array of notes
-// const data = require('./db/notes');
-
-// console.log('Hello Noteful!');
-
-// INSERT EXPRESS APP CODE HERE...
 const express = require('express');
 
 const data = require ('./db/notes');
 
 const app = express();
 
-app.listen(8080, function () {
+const { PORT } = require('./config');
+
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
