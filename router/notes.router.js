fa36f1ea9ac046a33270696eb44e7fb14027993e
Jordan Heffernan <jheff2@gmail.com>
2018-12-19 12:55:51 -0800

    added POST to notes router

--- router/notes.router.js
+++ router/notes.router.js
@@ -26,6 +26,28 @@ router.get('/notes/:id', (req, res, next) => {
   });
 });
 
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+  const newItem = { title, content };
+
+  if (!newItem.title) {
+    const err = new Error('Missing "title" in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/api/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
 router.put('/notes/:id', (req, res, next) => {
   const id = req.params.id;
 
